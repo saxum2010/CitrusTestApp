6b257200915450ce63d3881484f87a6e442330d7
saxum2010 <saxum2010@gmail.com>
2015-05-11 09:43:49 +0200

    update function datepicker

--- js/clean/events.js
+++ js/clean/events.js
@@ -465,6 +465,7 @@ $(document).on('click', '#personal_BirthDate', function() {
 	  mode: 'date'
 	};
 	datePicker.show(options, function(date){
-		personal_BirthDate = new Date(date).toString()val(date);
+		var dates = new Date(date).toString();
+		personal_BirthDate.val(dates);
 	});
 });
