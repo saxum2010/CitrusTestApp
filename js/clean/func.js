84588aaeded38cc4e90781b5edaf8075f7a91f81
saxum2010 <saxum2010@gmail.com>
2015-07-06 15:43:58 +0200

    fix url

--- js/clean/func.js
+++ js/clean/func.js
@@ -218,8 +218,8 @@ function LoadDefaultCatalog(category,position){
 					
 									
 				}else{									
-							
-				output += '<li class="'+lazy+'"><a data-transition="slide" data-ajax=false class="vclick_d_link"  link="#products-list?'+url+'"> 					<table style="width:100%"> 						<tr> 							<td style="vertical-align: middle;text-align:center;width:64px"  class="first"> 								<img src="' + value.image + '" >							 							</td> 							<td style="vertical-aling:middle;text-align:left;padding-left:1.1rem;"> 								<div class="box_catalog_status">'+text_flag+' </div><h2 class="item_name_only '+dop_class+'">' + value.name + '</h2> 							</td> 							<td style="width:25px"> 							</td> 						</tr> 					</table> 					 				</a></li>';
+				url = (value.link!= undefined)?'#products-list?'+url:"#product-card?product-id=" + value.id;
+				output += '<li class="'+lazy+'"><a data-transition="slide" data-ajax=false class="vclick_d_link"  link="'+url+'"> 					<table style="width:100%"> 						<tr> 							<td style="vertical-align: middle;text-align:center;width:64px"  class="first"> 								<img src="' + value.image + '" >							 							</td> 							<td style="vertical-aling:middle;text-align:left;padding-left:1.1rem;"> 								<div class="box_catalog_status">'+text_flag+' </div><h2 class="item_name_only '+dop_class+'">' + value.name + '</h2> 							</td> 							<td style="width:25px"> 							</td> 						</tr> 					</table> 					 				</a></li>';
 							}
 					
 			});	
