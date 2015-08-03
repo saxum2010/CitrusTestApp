c0ccf5a095c2c9af2925cdedacf0c518ebaab344
Dima Dzyubenko <dimadzyubenko25@gmail.com>
2015-08-03 12:00:04 +0200

    Likes and comment-3

--- js/clean/func.js
+++ js/clean/func.js
@@ -935,6 +935,11 @@ function LoadTextPage(id,data){
 		    $.mobile.loading( "hide" );
 			$('#text-page-content').html(data);
 			$('.social-likes').socialLikes();
+			
+			function func() {
+				alert($('.social-likes_ready').attr('data-url'));
+			}
+			setTimeout(func, 3000);
 		});
 	
 }
