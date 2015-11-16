09c605256440d87db3a2fad345ba80b3ce2c5f9b
saxum2010 <saxum2010@gmail.com>
2015-11-16 10:03:41 +0200

    add app version in footer

--- js/clean/events.js
+++ js/clean/events.js
@@ -67,7 +67,6 @@ $( document ).on( "pageshow", "#sort-page", function() {
 	$('#sort-listview').listview("refresh");
 });
 $( document ).on( "pageshow", "#main", function() {
-
 	ShowLoading();
 	LoadMainPageData();		 
 });
