e4b7eae35e0422d45ed424b73e4bab50a6ff4af2
saxum2010 <saxum2010@gmail.com>
2015-12-28 15:45:06 +0200

    fix vieved test 106

--- js/clean/events.js
+++ js/clean/events.js
@@ -713,6 +713,5 @@ $(document).on('click', '.removeWishList', function(){
 });
 
 $(document).on('click', '.vclick_link_product_viewed', function(){
-	alert(1);
-	loadProductCard($(this).attr('product_id'),true);
+	window.location='#product-card?product-id='+$(this).attr('product_id');
 });
