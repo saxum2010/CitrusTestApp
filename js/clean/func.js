9af29d296aaff754aa5aa5153d1416fa97f3fcbd
saxum2010 <saxum2010@gmail.com>
2015-12-28 16:18:37 +0200

    fix vieved test 111

--- js/clean/func.js
+++ js/clean/func.js
@@ -2247,7 +2247,7 @@ function showViewedProducts(datas, products_name){
 		if(parseInt(value.mprice, 10) > 1 && value.can_buy =="Y"){payment_parts = '<div class="catalog_payment_parts">Оплата частями</div>';}
 
 		row2 = (parseInt(value.mprice) > 1 && value.can_buy =="Y")?'<div class="price">'+value.price+' грн</div>':'<div class="status">'+can_buy_status+'</div>';
-		output += '<li class=""><a data-transition="slide" product_id="'+value.id+'" data-ajax=false onclick="alert(\''+url+'\');changePage(\''+url+'\');" link="'+url+'"><table style="width:100%"><tr><td style="vertical-align: middle;text-align:center;width:64px" class="first"><img src="' + value.image + '" ></td><td style="vertical-align:middle;text-align:left;padding-left:1.1rem;"><div class="box_catalog_status">'+text_flag+' </div><h2 class="item_name_only product">' + value.name + '</h2>'+row2+'<div class="props">'+prop+payment_parts+'</div></td><td style="width:25px"></td></tr></table></a></li>';
+		output += '<li class=""><a data-transition="slide" product_id="'+value.id+'" data-ajax=false onclick="changePage(\''+url+'\');" link="'+url+'"><table style="width:100%"><tr><td style="vertical-align: middle;text-align:center;width:64px" class="first"><img src="' + value.image + '" ></td><td style="vertical-align:middle;text-align:left;padding-left:1.1rem;"><div class="box_catalog_status">'+text_flag+' </div><h2 class="item_name_only product">' + value.name + '</h2>'+row2+'<div class="props">'+prop+payment_parts+'</div></td><td style="width:25px"></td></tr></table></a></li>';
 	});
 
 		products_wrap.html(output).listview("refresh").show();
