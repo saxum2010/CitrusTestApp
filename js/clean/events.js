a2a716348fa5dbff7d16353882ad1d75490f8a16
saxum2010 <saxum2010@gmail.com>
2015-12-31 11:48:55 +0200

    test 306

--- js/clean/events.js
+++ js/clean/events.js
@@ -71,7 +71,6 @@ $( document ).on( "pageshow", "#sort-page", function() {
 $( document ).on( "pageshow", "#main", function() {
 	ShowLoading();
 	LoadMainPageData();
-	MobileUser.basket.getViewedProducts(showViewedProductsOnMain);
 });
 
 $( document ).on( "pageshow", "#page-cart", function() {
